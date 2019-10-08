432c2aed85b9bf5ffd945d515c71b7bf733225b9
Joe Sweeney <joe@j1.io>
2019-10-08 00:24:17 -0400

    Add localstore.py

---
+++ localstore.py
@@ -0,0 +1,24 @@
+from typing import Callable, Iterable
+from render import MemeTemplate, TextBox
+
+class LocalTemplateStore:
+    def __init__(
+        self,
+        default_templates: Callable[[str], Iterable[MemeTemplate]],
+    ):
+        self.default_templates = default_templates
+
+    def refresh_memes(self, guild: str, hard: bool = False):
+        pass
+
+    def read_meme(self, guild: str, id: str, increment_use: bool = False) -> MemeTemplate:
+        return self.default_templates[None](id)
+
+    def list_memes(self, guild: str) -> Iterable[dict]:
+        templates = self.default_templates[None]
+        dicts = map(lambda t: t.serialize, templates)
+        dicts = map(lambda: d: {"name": t.name, "description":}, dicts)
+        return dicts
+
+    def guild_config(self, guild: str) -> dict:
+        pass
