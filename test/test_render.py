1ede7237799dae91e976d0d35b048fdb2873f42b
ironchefpython <ironchefpython>
2019-10-10 00:32:17 -0400

    switched to validating samples

--- test/test_render.py
+++ test/test_render.py
@@ -3,6 +3,7 @@ import os
 import sys
 import unittest
 import zlib
+import shlex
 from tempfile import NamedTemporaryFile
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -10,7 +11,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
 from localstore import LocalTemplateStore
-
+from generate_samples import SAMPLES
 
 class TestRender(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -19,20 +20,24 @@ class TestRender(unittest.TestCase):
         self.has_errors = False
 
     def test_simple(self):
-        template = self.store.read_meme(None, "bruh")
-        with io.BytesIO() as buffer:
-            template.render(("", "bruh"), buffer)
-            buffer.flush()
-            buffer.seek(0)
+        for filename, name, message in SAMPLES:
+            template = self.store.read_meme(None, name)
+            with open(f'samples/{filename}', 'rb') as f:
+                expected = zlib.adler32(f.read())
+            with io.BytesIO() as buffer:
+                template.render(shlex.split(message), buffer)
+                buffer.flush()
+                buffer.seek(0)
+                actual = zlib.adler32(buffer.read())
             try:
-                self.assertEqual(4024085169, zlib.adler32(buffer.read()))
+                self.assertEqual(expected, actual)
             except AssertionError:
                 if not self.has_errors:
                     self.has_errors = True
                     buffer.seek(0)
                     with NamedTemporaryFile(suffix=".png", delete=False) as f:
                         f.write(buffer.read())
-                        print(f"wrote image to {f.name}", file=sys.stderr)
+                        print(f"wrote failed image to {f.name}", file=sys.stderr)
                 raise
 
 
