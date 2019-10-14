fd73626e2dad0919872532174a8a5882f7f4f6ce
Sam <sam@samschmitt.com>
2019-10-13 22:41:06 -0400

    First complete terraform code

--- infrastructure/staging/staging.tf
+++ infrastructure/staging/staging.tf
@@ -1,21 +1,23 @@
 # Set AWS Provider, all options will be read from ENV
 provider "aws" { }
 
-# Need a VPC, subnet, interface, eip, security group
 
+# Basic VPC
 resource "aws_vpc" "staging-vpc" {
-cidr_block = "10.0.0.0/24"
-}
-
-resource "aws_subnet" "staging-subnet" {
-  vpc_id = "${aws_vpc.staging-vpc0}"
   cidr_block = "10.0.0.0/24"
 }
 
+# Should only need a single subnet
+resource "aws_subnet" "staging-subnet" {
+  vpc_id = "${aws_vpc.staging-vpc.id}"
+  cidr_block = "10.0.0.0/24"
+}
+
+# Two security groups for https and ssh
 resource "aws_security_group" "allow_https" {
   name = "Allow_https"
   description = "Allow HTTPS traffic"
-  vpc_id = "${aws_vpc.staging-vpc}"
+  vpc_id = "${aws_vpc.staging-vpc.id}"
 
   ingress {
     from_port = 443
@@ -36,7 +38,7 @@ resource "aws_security_group" "allow_https" {
 resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
   description = "Allow SSH Traffic"
-  vpc_id = "${aws_vpc.staging-vpc}"
+  vpc_id = "${aws_vpc.staging-vpc.id}"
 
   ingress {
     from_port = 22
@@ -51,20 +53,43 @@ resource "aws_security_group" "allow_ssh" {
 
   }
 }
-resource "aws_instance" "nachomeme" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
 
+# This will be the outputted IP
 resource "aws_eip" "ip" {
   instance = "${aws_instance.nachomeme.id}"
   vpc = true
 }
 
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] #Canonical
+
+
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.staging-vpc.id}"
+}
+
+
+# Main EC2 instance
 resource "aws_instance" "nachomeme" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "${data.aws_ami.latest-ubuntu.id}"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.allow_https}", "${aws_security_group.allow_ssh}"]
-  subnet_id = "${aws_subnet.staging-subnet}"
+  vpc_security_group_ids = ["${aws_security_group.allow_https.id}", "${aws_security_group.allow_ssh.id}"]
+  subnet_id = "${aws_subnet.staging-subnet.id}"
+}
+
+output "public_ip" {
+  value = aws_eip.ip.public_ip
 }
