2b54e31f64057542673420c056ec6819205698bd
ironchefpython <ironchefpython>
2019-10-22 23:47:36 -0400

    fixed import

--- generate_samples.py
+++ generate_samples.py
@@ -3,7 +3,7 @@ SAMPLES = (("bruh.png", "bruh", '"" / bruh'),)
 
 if __name__ == "__main__":
     import shlex
-    from local_store import LocalTemplateStore
+    from nachomemes.local_store import LocalTemplateStore
 
     store = LocalTemplateStore()
     for filename, name, message in SAMPLES:
