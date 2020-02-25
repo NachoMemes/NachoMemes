1819fbe6be24dec7ed108cd8270dd3bf25a083ba
Joe Sweeney <joe@j1.io>
2020-02-25 12:10:34 -0500

    Remove Git from run.py

--- run.py
+++ run.py
@@ -1,55 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Script to automate the running of the bot.
+""" 
+Script to automate the running of the bot.
 Handles the branch, debug and refresh state to run the bot in.
-Debug means it will run with test credentials and in verbose error mode. 
+Debug means it will run with test credentials and in verbose error mode.
 Refresh means it will HARD pull the branch you've chosen and run from there.
 """
 
 import argparse
-import os
 import subprocess
-import sys
 
-
-def main(branch=None, debug=False, refresh=False, local=False):
-    """ Builds and runs a list of processes.
+def main(debug=False, local=False):
+    """
+    Builds and runs a list of processes.
     """
 
-    if branch:
-        for c in (
-            "git reset HEAD --hard",
-            "git fetch",
-            f"git checkout {branch}",
-            "git pull",
-        ):
-            if c == "git reset HEAD --hard" and refresh == False:
-                continue
-            subprocess.call([c], shell=True)
-
-    runCommand = "python3 -m nachomemes.bot"
+    base_command = "python3 -m nachomemes.bot"
     if debug:
-        runCommand = runCommand + " --debug"
+        base_command = base_command + " --debug"
     if local:
-        runCommand = runCommand + " --local"
+        base_command = base_command + " --local"
 
-    subprocess.call([runCommand], shell=True)
-
+    subprocess.call([base_command], shell=True)
 
 if __name__ == "__main__":
-    # Setup and define the arg parser.
-    # This is so that we can take command line input.
     parser = argparse.ArgumentParser(
-        description="Runs the bot passed on input parameters."
+        description="Runs the bot with the passed in arguments."
     )
-    parser.add_argument(
-        "-b",
-        "--branch",
-        type=str,
-        action="store",
-        help="Optional. Which branch the bot should be run from.",
-    )
+
     parser.add_argument(
         "-d",
         "--debug",
@@ -58,15 +37,8 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "-r",
-        "--refresh",
-        action="store_true",
-        help="Whether or not to pull fresh from the branch",
-    )
-
-    parser.add_argument(
         "-l", "--local", action="store_true", help="Run locally without DynamoDB."
     )
 
     args = parser.parse_args()
-    main(args.branch, args.debug, args.refresh, args.local)
+    main(args.debug, args.local)
