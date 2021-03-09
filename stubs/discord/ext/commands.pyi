6873e0239086036ffd91599a399ff27f01d1176f
ironchefpython <ironchefpython@minime>
2021-03-09 18:42:19 -0500

    mavis beacon teaches typing

--- stubs/discord/ext/commands.pyi
+++ stubs/discord/ext/commands.pyi
@@ -1,10 +1,12 @@
 from discord import Guild
 
+from types import Any
+
 class Context: 
     @property
     def guild(self) -> Guild: ...
 
-    async def send(self, content=None, *, tts=False, embed=None, file=None,
-                                          files=None, delete_after=None, nonce=None,
-                                          allowed_mentions=None, reference=None,
-                                          mention_author=None): ...
+    async def send(self, content: Any=None, *, tts: bool=False, embed: Any=None, file: Any=None,
+                                          files: Any=None, delete_after: Any=None, nonce: Any=None,
+                                          allowed_mentions: Any=None, reference: Any=None,
+                                          mention_author: Any=None): ...
